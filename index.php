2981900a0da175679b385d70e6085a1dbe535f31
Mateus Craveiro <mccraveiro@gmail.com>
2012-04-19 10:34:58 -0300

    alterado o tamanho dos logos do apoio

--- index.php
+++ index.php
@@ -88,8 +88,15 @@
 			.patrocinio {
 				display: block;
 				margin: 15px auto;
-				max-height: 100px;
+				max-height: 110px;
 				max-width: 220px;
+			}
+
+			.apoio {
+				display: block;
+				margin: 15px auto;
+				max-height: 80px;
+				max-width: 160px;
 			}
 
 			.palestra {
@@ -150,20 +157,20 @@
 				<div class="side">
 					<div class="patrocinadores">Patrocinadores</div>
 					<a href="http://www.yazigi.com.br/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/logo-e6c24a57d4ab65cdc7cb04390c44098a.png" class="patrocinio" /></a>
-					<a href="http://www.everis.com/brazil/pt-BR/home/Paginas/home.aspx"><img src="<?php echo $urlbase; ?>/img/patrocinadores/logo_everis.png" class="patrocinio" /></a>
+					<a href="http://www.uol.com.br/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/uol-o-melhor-conteudo.png" class="patrocinio" /></a>
+					<a href="http://www.padtec.com.br/indexbr.php"><img src="<?php echo $urlbase; ?>/img/patrocinadores/logo_padtec.jpg" class="patrocinio" /></a>
+					<!-- <a href="http://www.everis.com/brazil/pt-BR/home/Paginas/home.aspx"><img src="<?php echo $urlbase; ?>/img/patrocinadores/logo_everis.png" class="patrocinio" /></a> -->
 				</div>
 				<div class="side">
 					<div class="patrocinadores">Apoio</div>
-					<a href="http://www.ufscar.br"><img src="<?php echo $urlbase; ?>/img/patrocinadores/22581_logoufscar.jpg" class="patrocinio" /></a>
-					<!--- Trocar logo <a href="http://plataformatec.com.br/"><img src="http://plataformatec.com.br/images/logo-home.png" class="patrocinio" /></a> -->
-					<a href="http://www.uol.com.br/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/uol-o-melhor-conteudo.png" class="patrocinio" /></a>
-					<a href="http://suanova.com/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/size_590_novapontocom.jpg" class="patrocinio" /></a>
-					<a href="http://novatec.com.br/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/logo_novatec.gif" class="patrocinio" /></a>
-					<a href="http://www.apontador.com.br/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/93746v2-max-250x250.png" class="patrocinio" /></a>
-					<a href="http://www.codeminer42.com/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/codeminer42.png" class="patrocinio" /></a>
-					<a href="http://www.cesar.org.br/site/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/logo.png" class="patrocinio" /></a>
-					<a href="http://www.padtec.com.br/indexbr.php"><img src="<?php echo $urlbase; ?>/img/patrocinadores/logo_padtec.jpg" class="patrocinio" /></a>
-					<a href="http://plataformatec.com.br/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/logo-home.png" class="patrocinio" /></a>
+					<a href="http://www.ufscar.br"><img src="<?php echo $urlbase; ?>/img/patrocinadores/22581_logoufscar.jpg" class="apoio" /></a>
+					<!--- Trocar logo <a href="http://plataformatec.com.br/"><img src="http://plataformatec.com.br/images/logo-home.png" class="apoio" /></a> -->
+					<a href="http://suanova.com/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/size_590_novapontocom.jpg" class="apoio" /></a>
+					<a href="http://novatec.com.br/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/logo_novatec.gif" class="apoio" /></a>
+					<a href="http://www.apontador.com.br/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/93746v2-max-250x250.png" class="apoio" /></a>
+					<a href="http://www.codeminer42.com/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/codeminer42.png" class="apoio" /></a>
+					<a href="http://www.cesar.org.br/site/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/logo.png" class="apoio" /></a>
+					<a href="http://plataformatec.com.br/"><img src="<?php echo $urlbase; ?>/img/patrocinadores/logo-home.png" class="apoio" /></a>
 					
 				</div>
 				<div class="clear"></div>
